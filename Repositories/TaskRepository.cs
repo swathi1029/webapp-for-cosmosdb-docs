1160d396c00c18ce242089f47ad5211df1a5fb50
swathi1029 <swathi1029@gmail.com>
2023-06-09 12:06:28 +0530

    changes done

--- Repositories/TaskRepository.cs
+++ Repositories/TaskRepository.cs
@@ -1,4 +1,7 @@
 ﻿using Azure.Identity;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Identity.Client;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
@@ -6,11 +9,47 @@ using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using SampleWebApi.Model;
 using SampleWebApi.Repositories;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using ClientCredential = Microsoft.Identity.Client.ClientCredential;
+using Microsoft.Azure.Documents;
+using Azure.Core;
+
+using Microsoft.Azure.Documents.Client;
+
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
 
 namespace SampleWebApi.Repositories
 {
-     
+    //public class AzureADAuthenticationHelper
+    //{
+    //    public async Task<string> GetAccessTokenAsync(string clientId, string clientSecret, string tenantId)
+    //    {
+    //        var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+    //        var tokenRequestContext = new TokenRequestContext(new[] { "https://vault.azure.net/.default" });
 
+    //        var token = await clientSecretCredential.GetTokenAsync(tokenRequestContext);
+    //        return token.Token;
+    //    }
+    //}
+    public class KeyVaultHelper
+    {
+        private readonly string keyVaultUrl;
+        private readonly TokenCredential tokenCredential;
+
+        public KeyVaultHelper(string keyVaultUrl, TokenCredential tokenCredential)
+        {
+            this.keyVaultUrl = keyVaultUrl;
+            this.tokenCredential = tokenCredential;
+        }
+
+        public string GetSecret(string secretName)
+        {
+            var secretClient = new SecretClient(new Uri(keyVaultUrl), tokenCredential);
+            var secret = secretClient.GetSecret(secretName);
+
+            return secret.Value.Value;
+        }
+    }
     public class TaskRepository : ITaskRepository
     {
         private string CosmosDbConnectionString;
@@ -21,21 +60,91 @@ namespace SampleWebApi.Repositories
         //public readonly string CosmosDbConnectionString = "AccountEndpoint=https://cosmosdbfordocs.documents.azure.com:443/;AccountKey=xgNu22pE5VOs2l2dpOSHdp27EhexeERGhrrvvE8Zzx1hrjCG60w2FC3B6yHFGmmm2kurfxfP0PSPACDbqOEQZg==";
         public readonly string CosmosDbName = "TasksManagementDB";
         public readonly string CosmosDbContainerName = "Tasks";
+        //private async Task<string> GetAccessTokenAsync()
+        //{
+        //    var clientId = "408a258e-cf9b-405c-9217-818982c9831a";
+        //    var clientSecret = "vkH8Q~8bew0i3zl0QAYtqdq6zTqI2LugcTBFraQU";
+
+        //    var authority = "https://login.microsoftonline.com/vkH8Q~8bew0i3zl0QAYtqdq6zTqI2LugcTBFraQU/";
+
+        //    var clientCredential = AuthenticationContext.AcquireTokenAsync(string resource, Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential clientCredential);
+        //    var authenticationContext = new AuthenticationContext(authority);
+
+        //    var result = await authenticationContext.AcquireTokenAsync("https://vault.azure.net", clientCredential);
+
+        //    return result.AccessToken;
+        //}
+        [Obsolete]
+        private async Task<string> GetAccessToken(string authority, string resource, string scope)
+        {
+
+            string clientId = "408a258e-cf9b-405c-9217-818982c9831a";// Configuration["ClientID"];
+            string clientSecret = "vkH8Q~8bew0i3zl0QAYtqdq6zTqI2LugcTBFraQU";
+            var tenantId = "a6b744ad-6d50-4ee1-b5d5-39e674f09e29";
+            var clientCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var authContext = new AuthenticationContext(authority);
+            var result = authContext.AcquireTokenAsync(resource, (IClientAssertionCertificate)clientCredential).GetAwaiter().GetResult();
+            return result.AccessToken;
+        }
+        public string GetVaultValue()
+        {
+            var client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessToken));
+            string vaultBaseUrl = "https://swathi1325908.vault.azure.net/";
+            string secretName = "cosmosdbfordocs-key";
+            var secret = client.GetSecretAsync(vaultBaseUrl, secretName).GetAwaiter().GetResult();
+            return secret.Value;
+
+        }
+        public string GetKeySecret()
+        {
+            DefaultAzureCredential credential = new DefaultAzureCredential();
+            Uri keyVaultUri = new Uri("https://swathi1325908.vault.azure.net/");
+
+            var client = new SecretClient(keyVaultUri, credential);
+
+            KeyVaultSecret secret = client.GetSecret("cosmosdbfordocs-endpoint");
+
+            var secretValue = secret.Value;
+            return secretValue;
+        }
 
         private Container GetContainerClient()
         {
-            var credential = new DefaultAzureCredential();
-            var keyVaultUrl = "https://swathi1325908.vault.azure.net/";
-            var secretClient = new SecretClient(new Uri(keyVaultUrl), credential);
+            //var clientId = "408a258e-cf9b-405c-9217-818982c9831a";
+            //var clientSecret = "vkH8Q~8bew0i3zl0QAYtqdq6zTqI2LugcTBFraQU";
+            ////var credential = new DefaultAzureCredential();
+            //var tenantId = "a6b744ad-6d50-4ee1-b5d5-39e674f09e29";
 
-            var cosmosDbConnectionStringSecretName = "cosmosdbfordocs-endpoint";
-            var cosmosDbKeySecretName = "cosmosdbfordocs-key";
+            ////var azureADHelper = new AzureADAuthenticationHelper();
+            ////var accessToken = await azureADHelper.GetAccessTokenAsync("408a258e-cf9b-405c-9217-818982c9831a", "vkH8Q~8bew0i3zl0QAYtqdq6zTqI2LugcTBFraQU", "a6b744ad-6d50-4ee1-b5d5-39e674f09e29");
+            //var keyVaultUrl = "https://swathi1325908.vault.azure.net/";
 
-            var cosmosDbConnectionStringSecret =  secretClient.GetSecret(cosmosDbConnectionStringSecretName);
-            var cosmosDbKeySecret =  secretClient.GetSecret(cosmosDbKeySecretName);
 
-            CosmosDbConnectionString = cosmosDbConnectionStringSecret.Value.Value;
-            CosmosDbKey = cosmosDbKeySecret.Value.Value;
+            //var client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessToken));
+            //string vaultBaseUrl = appSettings.Value.vaultBaseUrl;
+            //string secretName = appSettings.Value.secretName;
+            //var secret = client.GetSecretAsync(vaultBaseUrl, secretName).GetAwaiter().GetResult();
+            //return secret.Value;
+
+            //var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            //var keyVaultHelper = new KeyVaultHelper(keyVaultUrl, clientSecretCredential);
+            //var client = new SecretClient(new Uri(keyVaultUrl), clientSecretCredential);
+
+
+            //var secreturi = "cosmosdbfordocs-endpoint";
+            var secretValue = GetKeySecret();
+            // var secretkey = "cosmosdbfordocs-key";
+            var secretkeyval = GetVaultValue();
+
+
+            //var cosmosDbConnectionStringSecretName = "cosmosdbfordocs-endpoint";
+            //var cosmosDbKeySecretName = "cosmosdbfordocs-key";
+
+            //var cosmosDbConnectionStringSecret =  secretClient.GetSecret(cosmosDbConnectionStringSecretName);
+           // var cosmosDbKeySecret =  secretClient.GetSecret(cosmosDbKeySecretName);
+
+            CosmosDbConnectionString = secretValue;
+            CosmosDbKey = secretkeyval;
         
 
 
@@ -67,7 +176,7 @@ namespace SampleWebApi.Repositories
             task.Id = task.Id.ToString();   
 
             var container = GetContainerClient();
-            var response = await container.CreateItemAsync(task, new PartitionKey(task.Id));
+            var response = await container.CreateItemAsync(task, new Microsoft.Azure.Cosmos.PartitionKey(task.Id));
             return response.Resource;
            
 
@@ -81,7 +190,7 @@ namespace SampleWebApi.Repositories
 
             while (query.HasMoreResults)
             {
-                FeedResponse<TaskModel> response = await query.ReadNextAsync();
+                Microsoft.Azure.Cosmos.FeedResponse<TaskModel> response = await query.ReadNextAsync();
                 results.AddRange(response.ToList());
             }
 
@@ -93,7 +202,7 @@ namespace SampleWebApi.Repositories
             var container = GetContainerClient();
             try
             {
-                ItemResponse<TaskModel> response = await container.ReadItemAsync<TaskModel>(id, new PartitionKey(id));
+                ItemResponse<TaskModel> response = await container.ReadItemAsync<TaskModel>(id, new Microsoft.Azure.Cosmos.PartitionKey(id));
                 return response.Resource;
             }
             catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
@@ -105,7 +214,7 @@ namespace SampleWebApi.Repositories
         public async Task<TaskModel> UpdateTaskAsync(TaskModel task)
         {
             var container = GetContainerClient();
-            ItemResponse<TaskModel> response = await container.ReplaceItemAsync(task, task.Id, new PartitionKey(task.Id));
+            ItemResponse<TaskModel> response = await container.ReplaceItemAsync(task, task.Id, new Microsoft.Azure.Cosmos.PartitionKey(task.Id));
             
             return response.Resource;
         }
@@ -113,7 +222,7 @@ namespace SampleWebApi.Repositories
         public async Task DeleteTaskAsync(string id)
         {
             var container = GetContainerClient();
-            await container.DeleteItemAsync<TaskModel>(id, new PartitionKey(id));
+            await container.DeleteItemAsync<TaskModel>(id, new Microsoft.Azure.Cosmos.PartitionKey(id));
         }
     }
 }
